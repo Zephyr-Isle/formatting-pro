b491b2eb9b46e607c7d9a4f14bc61a0f3bcab141
Zephyr-Isle <zephyr_isle@outlook.com>
2026-06-29 13:36:48 +0800

    fix: Fix blank admin settings page by migrating to Flarum 2.0 API
    - Replace deprecated saveSettings with this.submitButton()
    - Use this.setting() and this.buildSettingComponent() for settings management
    - Remove manual app.data.settings state handling and Switch components
    - Use this.submitButton() instead of Button.component() for save action

--- js/src/admin/components/FormattingProSettingsPage.tsx
+++ js/src/admin/components/FormattingProSettingsPage.tsx
@@ -1,17 +1,11 @@
 import app from 'flarum/admin/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
-import Button from 'flarum/common/components/Button';
-import Switch from 'flarum/common/components/Switch';
-import saveSettings from 'flarum/admin/utils/saveSettings';
-import Stream from 'flarum/common/utils/Stream';
 
 export default class FormattingProSettingsPage extends ExtensionPage {
-  loading = false;
-  audioCssStream: Stream<string>;
-
   oninit(vnode: any) {
     super.oninit(vnode);
-    this.audioCssStream = new Stream(app.data.settings['zephyrisle-fof-formatting-pro.audio_css'] || '');
+
+    this.audioCssSetting = this.setting('zephyrisle-fof-formatting-pro.audio_css', '');
   }
 
   content() {
@@ -19,49 +13,34 @@ export default class FormattingProSettingsPage extends ExtensionPage {
       <div className="FormattingProSettingsPage">
         <div className="container">
           <h2>{app.translator.trans('zephyrisle-fof-formatting-pro.admin.settings.title')}</h2>
-          
+
           <div className="Form-group">
             <h3>{app.translator.trans('zephyrisle-fof-formatting-pro.admin.settings.plugins')}</h3>
-            
-            <div className="Checkbox">
-              <Switch 
-                state={app.data.settings['zephyrisle-fof-formatting-pro.plugin.autoaudio'] || false}
-                onchange={(value: boolean) => {
-                  app.data.settings['zephyrisle-fof-formatting-pro.plugin.autoaudio'] = value;
-                }}
-              >
-                {app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.autoaudio')}
-              </Switch>
-            </div>
 
-            <div className="Checkbox">
-              <Switch 
-                state={app.data.settings['zephyrisle-fof-formatting-pro.plugin.netease'] || false}
-                onchange={(value: boolean) => {
-                  app.data.settings['zephyrisle-fof-formatting-pro.plugin.netease'] = value;
-                }}
-              >
-                {app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.netease')}
-              </Switch>
-            </div>
+            {this.buildSettingComponent({
+              setting: 'zephyrisle-fof-formatting-pro.plugin.autoaudio',
+              label: app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.autoaudio'),
+              type: 'boolean',
+            })}
 
-            <div className="Checkbox">
-              <Switch 
-                state={app.data.settings['zephyrisle-fof-formatting-pro.plugin.bilibili'] || false}
-                onchange={(value: boolean) => {
-                  app.data.settings['zephyrisle-fof-formatting-pro.plugin.bilibili'] = value;
-                }}
-              >
-                {app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.bilibili')}
-              </Switch>
-            </div>
+            {this.buildSettingComponent({
+              setting: 'zephyrisle-fof-formatting-pro.plugin.netease',
+              label: app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.netease'),
+              type: 'boolean',
+            })}
+
+            {this.buildSettingComponent({
+              setting: 'zephyrisle-fof-formatting-pro.plugin.bilibili',
+              label: app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.bilibili'),
+              type: 'boolean',
+            })}
           </div>
 
           <div className="Form-group">
             <h3>{app.translator.trans('zephyrisle-fof-formatting-pro.admin.settings.audio_css')}</h3>
-            <textarea 
+            <textarea
               className="FormControl"
-              bidi={this.audioCssStream}
+              bidi={this.audioCssSetting}
               placeholder={app.translator.trans('zephyrisle-fof-formatting-pro.admin.settings.audio_css_placeholder')}
               rows={10}
             />
@@ -70,38 +49,9 @@ export default class FormattingProSettingsPage extends ExtensionPage {
             </p>
           </div>
 
-          <div className="Form-group">
-            {Button.component({
-              type: 'submit',
-              className: 'Button Button--primary',
-              loading: this.loading,
-              onclick: this.onsubmit.bind(this),
-            }, app.translator.trans('zephyrisle-fof-formatting-pro.admin.buttons.save'))}
-          </div>
+          <div className="Form-group">{this.submitButton()}</div>
         </div>
       </div>
     );
-  }
-
-  onsubmit(e: Event) {
-    e.preventDefault();
-    this.loading = true;
-
-    const settings = {
-      'zephyrisle-fof-formatting-pro.plugin.autoaudio': app.data.settings['zephyrisle-fof-formatting-pro.plugin.autoaudio'] || false,
-      'zephyrisle-fof-formatting-pro.plugin.netease': app.data.settings['zephyrisle-fof-formatting-pro.plugin.netease'] || false,
-      'zephyrisle-fof-formatting-pro.plugin.bilibili': app.data.settings['zephyrisle-fof-formatting-pro.plugin.bilibili'] || false,
-      'zephyrisle-fof-formatting-pro.audio_css': this.audioCssStream(),
-    };
-
-    saveSettings(settings)
-      .then(() => {
-        this.loading = false;
-        m.redraw();
-      })
-      .catch(() => {
-        this.loading = false;
-        m.redraw();
-      });
   }
 }
