b9e1b563147fcf95a6f769e4b7ecfd1aa98b55bb
Zephyr-Isle <zephyr_isle@outlook.com>
2026-06-30 18:18:57 +0800

    feat(admin): 添加扩展标识与新的管理后台配置项
    移除自定义的管理后台设置页面，改用内置扩展器添加配置项；为composer.json添加扩展唯一标识ID，新增自动音频、网易云音乐、B站插件的启用开关，以及自定义音频播放器CSS的文本配置项，并更新编译后的前端静态文件。

--- js/src/admin/extend.ts
+++ js/src/admin/extend.ts
@@ -1,7 +1,27 @@
 import Extend from 'flarum/common/extenders';
-import FormattingProSettingsPage from './components/FormattingProSettingsPage';
+import app from 'flarum/admin/app';
 
 export default [
-  new Extend.Admin() //
-    .page(FormattingProSettingsPage),
+  new Extend.Admin()
+    .setting(() => ({
+      setting: 'zephyrisle-fof-formatting-pro.plugin.autoaudio',
+      label: app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.autoaudio', {}, true),
+      type: 'boolean',
+    }))
+    .setting(() => ({
+      setting: 'zephyrisle-fof-formatting-pro.plugin.netease',
+      label: app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.netease', {}, true),
+      type: 'boolean',
+    }))
+    .setting(() => ({
+      setting: 'zephyrisle-fof-formatting-pro.plugin.bilibili',
+      label: app.translator.trans('zephyrisle-fof-formatting-pro.admin.plugins.bilibili', {}, true),
+      type: 'boolean',
+    }))
+    .setting(() => ({
+      setting: 'zephyrisle-fof-formatting-pro.audio_css',
+      label: app.translator.trans('zephyrisle-fof-formatting-pro.admin.settings.audio_css', {}, true),
+      help: app.translator.trans('zephyrisle-fof-formatting-pro.admin.settings.audio_css_help', {}, true),
+      type: 'textarea',
+    })),
 ];
